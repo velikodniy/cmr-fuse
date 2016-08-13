e3e30f0b6bebf12de285328814b824d0a9324b42
Vadim Velikodniy <vadim@velikodniy.name>
2016-08-13 23:31:32 +0300

    Mark arguments of some functions as unused to prevent warnings

--- filelist_cache.c
+++ filelist_cache.c
@@ -2,38 +2,40 @@
 
 bool keyeq_func(const HTableNode* a_, const HTableNode* b_, void *arg)
 {
-    filelist_cache_data_t* a = (filelist_cache_data_t*)a_;
-    filelist_cache_data_t* b = (filelist_cache_data_t*)b_;
-    return (strcmp(a->filename, b->filename) == 0);
+  (void) arg;
+  filelist_cache_data_t* a = (filelist_cache_data_t*)a_;
+  filelist_cache_data_t* b = (filelist_cache_data_t*)b_;
+  return (strcmp(a->filename, b->filename) == 0);
 }
 
 uint32_t hash_func(const HTableNode* a_, void *arg)
 {
-    filelist_cache_data_t* a = (filelist_cache_data_t*)a_;
-    return htable_default_hash(a->filename, strlen(a->filename));
+  (void) arg;
+  filelist_cache_data_t* a = (filelist_cache_data_t*)a_;
+  return htable_default_hash(a->filename, strlen(a->filename));
 }
 
 void* alloc_func(size_t size, void *arg)
 {
-    return malloc(size);
+  (void) arg;
+  return malloc(size);
 }
 
 void free_func(void* mem, void *arg)
 {
-    free(mem);
+  (void) arg;
+  free(mem);
 }
 
 void filelist_cache_create(HTable *hTable) {
-    htable_create(
-            hTable,
-            sizeof(filelist_cache_data_t),
-            hash_func,
-            keyeq_func,
-            alloc_func,
-            free_func,
-            NULL,
-            NULL
-    );
+  htable_create(hTable,
+		sizeof(filelist_cache_data_t),
+		hash_func,
+		keyeq_func,
+		alloc_func,
+		free_func,
+		NULL,
+		NULL);
 }
 
 void filelist_cache_free(HTable *hTable) {
